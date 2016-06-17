f2005e8199e975432306f192953de91ea48e2fae
bohaohan <54bohaohan@gmail.com>
2016-06-18 00:31:42 +0800

    modify

--- test_ws.py
+++ test_ws.py
@@ -99,6 +99,151 @@ def send(data, socket):
     return True
 
 
+def recv_data(num, con):
+    try:
+        all_data = con.recv(num)
+        if not len(all_data):
+            return False
+    except:
+        return False
+    # else:
+    #     code_len = ord(all_data[1]) & 127
+    # if code_len == 126:
+    #     masks = all_data[4:8]
+    #     data = all_data[8:]
+    # elif code_len == 127:
+    #     masks = all_data[10:14]
+    #     data = all_data[14:]
+    # else:
+    #     masks = all_data[2:6]
+    #     data = all_data[6:]
+    # raw_str = ""
+    # i = 0
+    # for d in data:
+    #     raw_str += chr(ord(d) ^ ord(masks[i % 4]))
+    # i += 1
+    print ord(all_data)
+    # return chr(all_data)
+    # data_length = con.recv(1)
+    # data_lengths = struct.unpack("B", data_length)
+    # data_length = data_lengths[0]&0b01111111
+    # masking = data_lengths[0] >> 7
+    # if data_length <= 125:
+    #     payloadLength = data_length
+    # elif data_length==126:
+    #     payloadLength = struct.unpack("H", con.recv(2))[0]
+    # elif data_length==127:
+    #     payloadLength = struct.unpack("Q", con.recv(8))[0]
+    # print "字符串长度是:%d"%(data_length,)
+    # if masking==1:
+    #     maskingKey = con.recv(4)
+    #     maskingKey = maskingKey
+    # data = con.recv(payloadLength)
+    # if masking==1:
+    #     i = 0
+    #     true_data = ''
+    #     for d in data:
+    #         true_data += chr(ord(d) ^ ord(maskingKey[i % 4]))
+    #         i += 1
+    #     print true_data
+    # else:
+    #     print data
+g_code_length = 0
+g_header_length = 0
+
+def get_datalength(msg):
+    global g_code_length
+    global g_header_length
+
+    # print (len(msg))
+    g_code_length = ord(msg[1]) & 127
+    received_length = 0
+    if g_code_length == 126:
+        #g_code_length = msg[2:4]
+        #g_code_length = (ord(msg[2])<<8) + (ord(msg[3]))
+        g_code_length = struct.unpack('>H', str(msg[2:4]))[0]
+        g_header_length = 8
+    elif g_code_length == 127:
+        #g_code_length = msg[2:10]
+        g_code_length = struct.unpack('>Q', str(msg[2:10]))[0]
+        g_header_length = 14
+    else:
+        g_header_length = 6
+    g_code_length = int(g_code_length)
+    return g_code_length
+
+
+def parse_data(msg):
+    global g_code_length
+    g_code_length = ord(msg[1]) & 127
+    received_length = 0
+    if g_code_length == 126:
+        g_code_length = struct.unpack('>H', str(msg[2:4]))[0]
+        masks = msg[4:8]
+        data = msg[8:]
+    elif g_code_length == 127:
+        g_code_length = struct.unpack('>Q', str(msg[2:10]))[0]
+        masks = msg[10:14]
+        data = msg[14:]
+    else:
+        masks = msg[2:6]
+        data = msg[6:]
+
+
+    i = 0
+    raw_str = ''
+
+
+    for d in data:
+        raw_str += chr(ord(d) ^ ord(masks[i%4]))
+        i += 1
+
+
+    # print (u"总长度是：%d" % int(g_code_length))
+    return raw_str
+
+
+def hex2dec(string_num):
+    return str(int(string_num.upper(), 16))
+
+
+def recv(conn):
+    length_buffer = 0
+    buffer = ""
+    buffer_utf8 = ""
+    global g_code_length
+    global g_header_length
+    mm = conn.recv(128)
+    if len(mm) <= 0:
+        return
+    if g_code_length == 0:
+        get_datalength(mm)
+    #接受的长度
+    length_buffer = length_buffer + len(mm)
+    buffer = buffer + mm
+    if length_buffer - g_header_length < g_code_length:
+        return
+    else :
+        buffer_utf8 = parse_data(buffer) #utf8
+        msg_unicode = str(buffer_utf8).decode('utf-8', 'ignore') #unicode
+        # print "receive", msg_unicode
+        g_code_length = 0
+        return msg_unicode
+        # if msg_unicode=='quit':
+        #     print (u'Socket%s Logout!' % (index))
+        #     nowTime = time.strftime('%H:%M:%S',time.localtime(time.time()))
+        #     sendMessage(u'%s %s say: %s' % (nowTime, self.remote, self.name+' Logout'))
+        #     deleteconnection(str(self.index))
+        #     self.conn.close()
+        #     break #退出线程
+        # else:
+        #     #print (u'Socket%s Got msg:%s from %s!' % (self.index, msg_unicode, self.remote))
+        #     nowTime = time.strftime(u'%H:%M:%S',time.localtime(time.time()))
+        #     sendMessage(u'%s %s say: %s' % (nowTime, self.remote, msg_unicode))
+        #重置buffer和bufferlength
+
+
+
 # handshake
 def handshake(con):
     headers = {}
@@ -125,6 +270,18 @@ def handshake(con):
     con.send(str_handshake)
 
     send("hello world", con)
+    while True:
+        recvd = recv(con)
+        print "receive", recvd
+        send(recvd, con)
+    # while True:
+    #     message = con.recv(1024)
+    #     if repr(message) == '':
+    #         print "客户端断开链接"
+    #         return
+    #     if not message:
+    #         print "receive", message
+    #         send(message, con)
     # con.send("hello world")
 
     return True
