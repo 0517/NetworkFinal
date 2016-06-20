b499d836e74daa13c68d55129d7c43c31986db44
吴思瀚 <qm@wusihandeMacBook-Pro.local>
2016-06-20 22:55:18 +0800

    sdlkalsfjlaje;f

--- WebSocketServer.py
+++ WebSocketServer.py
@@ -12,6 +12,7 @@ import shutil
 import json
 from ip_management import IpListManagement
 import sys
+from log.log import LoggingManagement
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -33,10 +34,10 @@ class DataSocket(threading.Thread):
                     data_sock = WebSocket(data_sock)
                     data_sock.isData = True
                 print 'receive data socket from', client_addr
-            except socket.timeout:
-                pass
-            except socket.error:
-                break
+            except socket.timeout as e:
+                logging.DEBUG(e)
+            except socket.error as e:
+                logging.error(e)
             else:
                 if self.server.data_socket_for == 'NLST':
                     if self.server.nlst_data_socket is not None:
@@ -63,12 +64,6 @@ class DataSocket(threading.Thread):
                     else:
                         self.server.stor_data_socket = data_sock
 
-                # if self.server.dataSock is not None:
-                #     self.server.dataSock.close()
-                #     self.server.dataSock = data_sock
-                # else:
-                #     self.server.dataSock = data_sock
-
 
 class Server(threading.Thread):
 
@@ -84,7 +79,6 @@ class Server(threading.Thread):
             self.controlSock = WebSocket(controlSock, self.bufSize)
         self.clientAddr = clientAddr
         self.dataListenSock = None
-        # self.dataSock = None
         self.dataAddr = '127.0.0.1'
         self.dataPort = None
         self.username = ''
@@ -103,69 +97,75 @@ class Server(threading.Thread):
 
     def run(self):
 
-        self.controlSock.send(b'220 Service ready for new user.\r\n')
+        self.controlSock.send('220 Service ready for new user.\r\n')
 
         while True:
 
             cmd = self.controlSock.recv(self.bufSize)
 
             if cmd == '':
+                self.controlSock.send('221 Serviceclosingcontrolconnection.\r\n')
                 self.controlSock.close()
                 break
-
-            cmdHead = cmd.split()[0].upper()
+            try:
+                cmdHead = cmd.split()[0].upper()
+            except Exception as e:
+                logging.error(e)
+                self.controlSock.send('221Serviceclosingcontrolconnection.\r\n')
+                self.controlSock.close()
+                break
             print 'receive head', cmdHead
             if cmdHead == 'QUIT':
-                self.controlSock.send(b'221 Service closing control connection.\r\n')
+                self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
                 self.controlSock.close()
                 break
 
             elif cmdHead == 'HELP':
-                self.controlSock.send(b'214 QUIT HELP USER PASS PWD CWD TYPE PASV NLST RETR STOR\r\n')
+                self.controlSock.send('214 QUIT HELP USER PASS PWD CWD TYPE PASV NLST RETR STOR\r\n')
 
             elif cmdHead == 'USER':
 
                 if len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
 
                 else:
                     self.username = cmd.split()[1]
-                    self.controlSock.send(b'331 User name okay, need password.\r\n')
+                    self.controlSock.send('331 User name okay, need password.\r\n')
 
             elif cmdHead == 'PASS':
 
                 if self.username == '':
-                    self.controlSock.send(b'503 Bad sequence of commands.\r\n')
+                    self.controlSock.send('503 Bad sequence of commands.\r\n')
 
                 else:
 
                     if len(cmd.split()) < 2:
-                        self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                        self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
 
                     else:
                         if self.username == 'administrator' and cmd.split()[1] == 'administrator':
                             self.if_administrator = True
-                        self.controlSock.send(b'230 User logged in, proceed.\r\n')
+                        self.controlSock.send('230 User logged in, proceed.\r\n')
                         self.authenticated = True
 
             elif cmdHead == 'PWD':
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 else:
                     self.controlSock.send('257 "%s" is the current directory.\r\n' % self.cwd)
 
             elif cmdHead == 'CDUP':
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 else:
                     try:
                         os.chdir("..")
                     except OSError as e:
                         print e
-                        self.controlSock.send(b'550 Requested action not taken. File unavailable (e.g., file busy).\r\n')
+                        self.controlSock.send('550 Requested action not taken. File unavailable (e.g., file busy).\r\n')
                     else:
                         self.cwd = os.getcwd()
                         self.controlSock.send('250 "%s" is the current directory.\r\n' % self.cwd)
@@ -173,17 +173,17 @@ class Server(threading.Thread):
 
             elif cmdHead == 'RMD':
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
                 else:
                     rm_dir = cmd.split()[1]
                     os.chdir(self.cwd)
                     try:
                         shutil.rmtree(rm_dir)
                     except Exception as e:
-                        print e
+                        logging.error(e)
                         self.controlSock.send('550 Requested action not taken.')
                     else:
                         self.controlSock.send('250 Requested file action okay, completed.')
@@ -191,17 +191,17 @@ class Server(threading.Thread):
 
             elif cmdHead == 'MKD':
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
                 else:
                     new_dir = cmd.split()[1]
                     os.chdir(self.cwd)
                     try:
                         os.mkdir(new_dir)
                     except Exception as e:
-                        print e
+                        logging.error(e)
                         self.controlSock.send('550 Requested action not taken.')
                     else:
                         self.controlSock.send('250 Requested file action okay, completed.')
@@ -210,17 +210,17 @@ class Server(threading.Thread):
             elif cmdHead == 'DELETE':
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
                 else:
                     os.chdir(self.cwd)
                     delete_dir = cmd.split()[1]
                     try:
                         os.remove(delete_dir)
                     except Exception as e:
-                        print e
+                        logging.error(e)
                         self.controlSock.send('550 Requested action not taken.')
                     else:
                         self.controlSock.send('250 Requested file action okay, completed.')
@@ -229,7 +229,7 @@ class Server(threading.Thread):
             elif cmdHead == 'CWD':
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
                     self.controlSock.send('250 "%s" is the current directory.\r\n' % self.cwd)
@@ -241,8 +241,8 @@ class Server(threading.Thread):
                     try:
                         os.chdir(next_dir)
                     except OSError as e:
-                        print e
-                        self.controlSock.send(b'550 Requested action not taken. File unavailable (e.g., file busy).\r\n')
+                        logging.error(e)
+                        self.controlSock.send('550 Requested action not taken. File unavailable (e.g., file busy).\r\n')
 
                     else:
                         self.cwd = os.getcwd()
@@ -252,17 +252,17 @@ class Server(threading.Thread):
             elif cmd == 'TYPE':
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
                 else:
                     self.controlSock.send("502 Command not implemented\r\n")
 
             elif cmdHead == 'PASV':
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
 
                 elif cmd.split()[1] != 'NLST' and cmd.split()[1] != 'RETR' and cmd.split()[1] != 'STOR' and cmd.split()[1] != 'IP':
                     self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
@@ -282,7 +282,7 @@ class Server(threading.Thread):
 
             elif cmdHead == 'PORT':
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
                 else:
                     self.controlSock.send("502 Command not implemented\r\n")
 
@@ -291,7 +291,7 @@ class Server(threading.Thread):
                 time.sleep(0.5)
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif self.dataMode == 'PASV' and self.nlst_data_socket is not None:
                     self.controlSock.send(b'125 Data connection already open. Transfer starting.\r\n')
@@ -309,34 +309,34 @@ class Server(threading.Thread):
                     self.nlst_data_socket.send(directory)
                     self.nlst_data_socket.close()
                     self.nlst_data_socket = None
-                    self.controlSock.send(b'225 Closing data connection. Requested file action successful (for example, file transfer or file abort).\r\n')
+                    self.controlSock.send('225 Closing data connection. Requested file action successful (for example, file transfer or file abort).\r\n')
                     os.chdir(self.root_wd)
                 else:
-                    self.controlSock.send(b"425 Can't open data connection.\r\n")
+                    self.controlSock.send("425 Can't open data connection.\r\n")
 
             elif cmdHead == 'RETR':
 
                 time.sleep(0.5)
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
 
                 elif self.dataMode == 'PASV' and self.retr_data_socket is not None:
-                    self.controlSock.send(b'125 Data connection already open; transfer starting.\r\n')
+                    self.controlSock.send('125 Data connection already open; transfer starting.\r\n')
                     file_name = cmd.split()[1]
 
                     try:
                         os.chdir(self.cwd)
                         self.retr_data_socket.send(open(file_name, 'rb').read(), opcode=2)
                     except Exception as e:
-                        print e
+                        logging.error(e)
                         # 没有文件的话在此处理
-                        self.controlSock.send(b'550 Requested action not taken. File unavailable (e.g., file busy).\r\n')
+                        self.controlSock.send('550 Requested action not taken. File unavailable (e.g., file busy).\r\n')
                     else:
-                        self.controlSock.send(b'225 Closing data connection. Requested file action successful (for example, file transfer or file abort).\r\n')
+                        self.controlSock.send('225 Closing data connection. Requested file action successful (for example, file transfer or file abort).\r\n')
                     os.chdir(self.root_wd)
                     self.retr_data_socket.close()
                     self.retr_data_socket = None
@@ -348,13 +348,13 @@ class Server(threading.Thread):
                 time.sleep(0.5)
 
                 if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
+                    self.controlSock.send('530 Not logged in.\r\n')
 
                 elif len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
+                    self.controlSock.send('501 Syntax error in parameters or arguments.\r\n')
 
                 elif self.dataMode == 'PASV' and self.stor_data_socket is not None:
-                    self.controlSock.send(b'125 Data connection already open; transfer starting.\r\n')
+                    self.controlSock.send('125 Data connection already open; transfer starting.\r\n')
                     os.chdir(self.cwd)
                     file_name = cmd[cmd.index(' ')+1:]
                     f = open(file_name, 'ab+')
@@ -368,7 +368,8 @@ class Server(threading.Thread):
                             if data is None:
                                 continue
                             f.write(data)
-                        except socket.error:
+                        except socket.error as e:
+                            logging.error(e)
                             break
                     f.close()
                     print "success"
@@ -385,6 +386,7 @@ class Server(threading.Thread):
                         try:
                             self.ip_data_socket.send(json.dumps(self.ip_management.ip_data['ip_list']))
                         except Exception as e:
+                            logging.error(e)
                             self.controlSock.send('550 Requested action not taken.')
                         else:
                             self.controlSock.send('225 Closing data connection. Requested action successful.\r\n')
@@ -456,6 +458,7 @@ class FTPServer(threading.Thread):
                 Server(controlSock, clientAddr, False, os.getcwd(), self.ip_management).start()
 
 if __name__ == '__main__':
+    logging = LoggingManagement()
     ip_management = IpListManagement()
     FTPServer('Primitive', ip_management).start()
     FTPServer('Web', ip_management).start()
